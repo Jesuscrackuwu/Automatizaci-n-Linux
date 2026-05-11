bde304230cdc8575d0f8056a62ee293d71d42285
Jesuscrackuwu <119819621+Jesuscrackuwu@users.noreply.github.com>
2026-05-11 11:29:01 -0500

    Update test_chrome.py

--- test_chrome.py
+++ test_chrome.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import time
+import os
 
 # Configurar opciones de Chrome
 options = Options()
@@ -11,8 +12,8 @@ options.add_argument("--disable-gpu")
 options.add_argument("--remote-debugging-port=9222")
 options.add_argument("--window-size=1280,800")
 
-# Si estás en servidor sin entorno gráfico (como EC2)
-options.add_argument("--headless=new")  # ejecuta sin interfaz
+# Si estás en servidor sin entorno gráfico
+options.add_argument("--headless=new")
 
 # Ruta del ChromeDriver
 service = Service("/usr/local/bin/chromedriver")
@@ -21,12 +22,26 @@ print("🚀 Iniciando prueba con Google Chrome...")
 
 # Iniciar navegador
 driver = webdriver.Chrome(service=service, options=options)
-driver.get("https://www.google.com")
 
-print("✅ Página abierta:", driver.title)
+try:
+    # Abrir página
+    driver.get("https://portal.nuevaeps.com.co/Portal/home.jspx")
 
-# Esperar unos segundos y cerrar
-time.sleep(3)
-driver.quit()
+    print("✅ Página abierta:", driver.title)
+
+    # Esperar a que cargue bien
+    time.sleep(3)
+
+    # Ruta donde guardar captura
+    screenshot_path = os.path.join(os.getcwd(), "captura_google.png")
+
+    # Tomar captura
+    driver.save_screenshot(screenshot_path)
+
+    print(f"📸 Captura guardada en: {screenshot_path}")
+
+finally:
+    # Cerrar navegador
+    driver.quit()
 
 print("🎯 Prueba finalizada correctamente.")
